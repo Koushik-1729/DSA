498b83ec657a9d6968f3713bd76f11605bfeb9d2
Koushik-1729 <83219883+Koushik-1729@users.noreply.github.com>
2024-06-18 10:16:26 +0530

    map

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,21 +1,53 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// // the max size we declare in side main is 10**6;
+// //if we declare greater then 10**6 it will throw segmentation error
+
+// int main()
+// {
+//   int n;
+//   cin>>n;
+//   int arr[n];
+//   for(int i=0;i<n;i++){
+//     cin>>arr[i];
+
+//   }
+//   int hash[13]={0};
+//   for(int i=0;i<n;i++)
+//   {
+//     hash[arr[i]]+=1;
+//   }
+//   int q;
+//   cin>>q;
+//   while(q--)
+//   {
+//     int number;
+//     cin>>number;
+//     cout<<hash[number]<<endl;
+//   }
+//   return 0;
+
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
-// the max size we declare in side main is 10**6;
-//if we declare greater then 10**6 it will throw segmentation error
-
 int main()
 {
   int n;
-  cin>>n;
+  cin >> n;
   int arr[n];
-  for(int i=0;i<n;i++){
+  for(int i=0;i<n;i++)
+  {
     cin>>arr[i];
 
   }
-  int hash[13]={0};
+  //pre computing
+  map<int,int>mpp;
   for(int i=0;i<n;i++)
   {
-    hash[arr[i]]+=1;
+    mpp[arr[i]]++;
+
   }
   int q;
   cin>>q;
@@ -23,8 +55,13 @@ int main()
   {
     int number;
     cin>>number;
-    cout<<hash[number]<<endl;
+    cout<<mpp[number]<<endl;
+
+
   }
   return 0;
+
+
+
 
 }
